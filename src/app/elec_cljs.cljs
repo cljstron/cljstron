325a3cbe24bd8f8c5beec35f78182fc9d71f794e
Ivan Pierre <me@ivanpierre.ch>
2017-10-14 12:27:54 +0200

    adding keys to create window

--- src/app/elec_cljs.cljs
+++ src/app/elec_cljs.cljs
@@ -3,19 +3,51 @@
             [cljs.nodejs :as js]
             [clojure.string :refer [join]]))
 
-; enable printf redirect to the console
-(enable-console-print!)
-
 (defn- file-url [filename]
-  "return normalized url from `filename`"
-  (str "file://" (join "/" [(js* "__dirname") ".." "resources" filename])))
+  "return normalized url from `filename` pointing to a file in the resources directory."
+ (str "file://" (join "/" [(js* "__dirname") ".." "resources" filename])))
 
-(def main-window (atom nil)) 
+(def 
+  ^{:doc "Var bound to the currently open windows.
+  `keys` are unique `Keyword` of the window, `values` are `BrowerWindow` instances"
+    :jsdoc ["@type {*}"]}
+  main-windows (atom {})) 
+(defn drop-window [key]
+  "Drop `key` entry from main-windows"
+  (swap! main-windows dissoc key)
+  key)
 
-(defn create-window []  
-  (println "create-window")
-  (reset! main-window (BrowserWindow. {:witdh 800 :height 600}))
-  (.loadURL @main-window (file-url "index.html"))
-  (.on @main-window "close" 
-    (fn [] (reset! main-window nil))))
+(defn add-window [key window]
+  "`window` @ `key` in main-windows' map"
+  (swap! main-windows merge {key window})
+  key)
 
+(defn get-window [key]
+  "Get the `key` window."
+  (get @main-windows key))
+
+(defn load-window [key url]
+  "Load html page `url` in the `key` window."
+  (.loadURL (get-window key) (file-url url))
+  key)
+
+(defonce window-keys
+  #{:width :height :show :backgroundColor :parent :modal :x :y 
+    :useContentSize :center :minWidth :minHeight :maxWidth :maxHeigth 
+    :resizable :movable :minimizable :maximizable :closable :focusable
+    :alwaysOnTop :fullscreen :fullscreenable :simpleFullscreen :skipTaskbar
+    :kiosk :title :icon :frame :acceptFirstMouse :disbleAutoHideCursor
+    :autoHideMenuBar :enableLargerThanScreen :hasShadow :opacity :darkTheme
+    :transparent :fullScreenWindowTitle :thickFrame :vibrancy :zoomToPageWidth
+    :webPreferences})
+
+(defn create-window 
+  [^Keyword key 
+    &{:as keys}]  
+  "Create `key` window from `keys` values and store it in main-windows"
+  (let [new-keys (conj {:witdh 800 :height 600} keys)
+        temp-win (BrowserWindow. new-keys)]
+    (swap! main-windows conj {key temp-win})
+    (.on temp-win "close" 
+      (fn [] (drop-window key))))
+  key)
--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,15 +1,17 @@
 (ns app.main
   (:require ["electron" :as electron :refer [app]] 
             [cljs.nodejs :as node :refer [enable-util-print!]]
-            [app.elec-cljs :refer [create-window]]))
+            [app.elec-cljs :refer [create-window load-window]]))
 
 (enable-util-print!)
 
+(defn load-main-window []
+  (-> :main
+    create-window
+    (load-window "index.html")))
+
 (defn ^:export init []
-  (println "(app.main/init)")
-  (.on app "ready" create-window)  
+  (.on app "ready" load-main-window)  
   (.on app "window-all-closed" 
     #(when (not= js/process.platrorm "darwin")
       (.quit app))))
-
-(set! *main-cli-fn* init)
